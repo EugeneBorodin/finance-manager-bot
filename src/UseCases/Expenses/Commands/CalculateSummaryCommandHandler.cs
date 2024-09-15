5ed9025e5f48a8a57117ab399d653dc8f4960a37
Eugene Borodin <borobit@gmail.com>
2024-09-16 01:38:19 +0300

    refactoring calculate summary command

--- src/UseCases/Expenses/Commands/CalculateSummaryCommandHandler.cs
+++ src/UseCases/Expenses/Commands/CalculateSummaryCommandHandler.cs
@@ -5,6 +5,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using UseCases.Models;
 
 namespace UseCases.Expenses.Commands;
 
@@ -40,29 +41,36 @@ public class CalculateSummaryCommandHandler : IRequestHandler<CalculateSummaryCo
                 .Where(e => e.DateTime >= request.StartDate &&
                             e.DateTime <= request.EndDate)
                 .GroupBy(e => e.Category)
-                .Select(group => new { Category = group.Key, Sum = group.Sum(e => e.Value) })
+                .Select(group => new ExpenseAggregationItem { Category = group.Key, Sum = group.Sum(e => e.Value) })
                 .ToListAsync(cancellationToken);
 
-            var sb = new StringBuilder();
-            decimal spent = 0;
-            sb.Append(
-                $"Сводка за период {request.StartDate.ToLocalTime():dd.MM.yyyy} - {request.EndDate.ToLocalTime():dd.MM.yyyy}\n\n");
-            sb.Append($"Кол-во денежных средств: {request.AccountBalance}\n\n");
-            sb.Append("Траты по категориям:\n");
-            foreach (var record in aggregation)
-            {
-                sb.Append($"{record.Category}: {record.Sum}\n");
-                spent += record.Sum;
-            }
-
-            sb.Append("\n");
-            sb.Append($"Потрачено денег: {spent}\n");
-            sb.Append($"Остаток на балансе: {request.AccountBalance - spent}");
-
-            _logger.LogInformation("Сводка сформирована по параметрам: {@request}. Остаток на балансе: {balance}",
-                request, request.AccountBalance - spent);
-
-            return sb.ToString();
+            var (report, balance) = CreateTextReport(aggregation, request.StartDate, request.EndDate, request.AccountBalance);
+            
+            _logger.LogInformation("Сводка сформирована по параметрам: {@request}. Остаток на балансе: {balance}", request, balance);
+            
+            return report;
         }
     }
+
+    private (string, decimal) CreateTextReport(IEnumerable<ExpenseAggregationItem> aggregation, DateTimeOffset startDate, DateTimeOffset endDate, decimal accountBalance)
+    {
+        var sb = new StringBuilder();
+        decimal spent = 0;
+        sb.Append(
+            $"Сводка за период {startDate.ToLocalTime():dd.MM.yyyy} - {endDate.ToLocalTime():dd.MM.yyyy}\n\n");
+        sb.Append($"Кол-во денежных средств: {accountBalance}\n\n");
+        sb.Append("Траты по категориям:\n\n");
+        foreach (var record in aggregation)
+        {
+            sb.Append($"{record.Category}: {record.Sum}\n");
+            spent += record.Sum;
+        }
+
+        sb.Append("\n");
+        sb.Append($"Потрачено денег: {spent}\n");
+        sb.Append($"Остаток на балансе: {accountBalance - spent}");
+
+        return (sb.ToString(), accountBalance - spent);
+    }
 }
+
---
+++ src/UseCases/Models/ExpenseAggregationItem.cs
@@ -0,0 +1,7 @@
+namespace UseCases.Models;
+
+public class ExpenseAggregationItem
+{
+    public string Category { get; set; }
+    public decimal Sum { get; set; }
+}
